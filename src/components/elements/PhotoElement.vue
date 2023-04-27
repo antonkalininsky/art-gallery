21f2b46fe7dabb774288b632dc2b4c7f1af0cd49
Anton <chadik.free@gmail.com>
2023-04-27 15:56:36 +0600

    Доработка компонента PhotoElement: выделение компонента PhotoPanelElement, переработка адаптивки

--- src/components/elements/PhotoElement.vue
+++ src/components/elements/PhotoElement.vue
@@ -1,11 +1,8 @@
 <script setup>
+import PhotoPanelElement from "./PhotoPanelElement.vue";
 import { useGeneralStore } from "../../stores/general";
 const store = useGeneralStore();
 const props = defineProps(["data"]);
-
-function openInNewTab() {
-    window.open(props.data.urls.regular);
-}
 </script>
 
 <template lang="">
@@ -13,63 +10,8 @@ function openInNewTab() {
         <div class="photo__bg-wrap">
             <div class="photo__bg"></div>
         </div>
-
         <div class="photo__inner wrap-prop">
-            <div class="photo__panel panel text">
-                <div class="panel__author">
-                    <div class="panel__avatar">
-                        <img
-                            :src="props.data.user.profile_image.medium"
-                            alt=""
-                            class="panel__img"
-                        />
-                    </div>
-                    <div class="panel__text">
-                        <div class="panel__name">
-                            {{ props.data.user.name }}
-                        </div>
-                        <div
-                            class="panel__tag"
-                            v-if="props.data.user.instagram_username !== null"
-                        >
-                            @{{ props.data.user.instagram_username }}
-                        </div>
-                    </div>
-                </div>
-                <div class="panel__buttons">
-                    <button
-                        class="button button_favourite text"
-                        @click="store.toggleFavourite(props.data.id)"
-                        :class="{
-                            button_favourite_true: store.checkFavourite(
-                                props.data.id
-                            ),
-                        }"
-                    >
-                        <div class="button__inner">
-                            <img
-                                src="@/assets/img/icons/heart-black.svg"
-                                alt=""
-                                srcset=""
-                            />
-                        </div>
-                    </button>
-                    <button
-                        class="button button_download text"
-                        @click="openInNewTab()"
-                    >
-                        <div class="button__inner">
-                            <img
-                                src="@/assets/img/icons/download.svg"
-                                alt=""
-                                srcset=""
-                                class="button__img"
-                            />
-                            <span class="button__text"> Download </span>
-                        </div>
-                    </button>
-                </div>
-            </div>
+            <PhotoPanelElement class="photo__panel" :data="props.data" />
             <img :src="props.data.urls.regular" alt="" class="photo__img" />
         </div>
     </div>
@@ -86,6 +28,7 @@ function openInNewTab() {
     height: 100%;
     width: 100%;
     overflow: hidden;
+    z-index: -100;
 }
 
 .photo__bg {
@@ -106,49 +49,6 @@ function openInNewTab() {
 .photo__inner {
     z-index: 9999;
 }
-
-.panel {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 40px 0;
-}
-
-.panel__author {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-}
-
-.panel__avatar {
-    border-radius: 8px;
-    overflow: hidden;
-    width: 55px;
-    height: 55px;
-}
-
-.panel__name {
-    font-family: "Roboto";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 30px;
-    line-height: 35px;
-}
-
-.panel__text {
-    color: white;
-    padding-left: 10px;
-}
-
-.panel__img {
-    width: 100%;
-    height: auto;
-}
-
-.panel__buttons {
-    height: min-content;
-}
 .photo__img {
     width: 100%;
     height: auto;
@@ -157,109 +57,13 @@ function openInNewTab() {
     box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.5);
 }
 
-.button {
-    background: #ffffff;
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 8px;
-    border: 0;
-    margin-left: 20px;
-    cursor: pointer;
-}
-
-.button__inner {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.button__img {
-    margin-right: 16px;
-}
-
-.button_favourite {
-    width: 49.07px;
-    height: 49.07px;
-}
-
-.button_favourite:hover {
-    background: #ccc;
-}
-
-.button_favourite_true {
-    background-color: rgb(81, 176, 81);
-}
-
-.button_favourite_true:hover {
-    background-color: rgb(217, 56, 56);
-}
-
-.button_download {
-    width: 206.25px;
-    height: 49.62px;
-    background-color: #fff200;
-
-    font-family: "Roboto";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 23px;
-
-    color: #000000;
-}
-
-.button_download:hover {
-    background-color: #989001;
-}
-
-@media screen and (max-width: 1050px) {
-    .panel__text {
-        color: #000;
-    }
-
-    .photo__img {
-        box-shadow: unset;
-    }
-
-    .panel__avatar {
-        width: 48px;
-        height: 48px;
-    }
-
+@media (max-width: 992px) {
     .photo__bg {
         display: none;
     }
 
-    .panel__name {
-        font-size: 18px;
-    }
-
-    .panel__tag {
-        font-size: 14px;
-        color: #bdbdbd;
-    }
-
-    .button_download {
-        margin-left: 16px;
-    }
-    .button_favourite {
-        margin-left: 0;
-    }
-}
-
-@media screen and (max-width: 700px) {
-    .button_download,
-    .button_favourite {
-        width: 41px;
-        height: 41px;
-    }
-
-    .button__text {
-        display: none;
-    }
-
-    .button__img {
-        margin: 0;
+    .photo__img {
+        box-shadow: none;
     }
 }
 </style>
---
+++ src/components/elements/PhotoPanelElement.vue
@@ -0,0 +1,190 @@
+<script setup>
+import { useGeneralStore } from "../../stores/general";
+const store = useGeneralStore();
+
+const props = defineProps(["data"]);
+
+function openInNewTab() {
+    window.open(props.data.urls.regular);
+}
+</script>
+
+<template>
+    <div class="panel text">
+        <div class="panel__author">
+            <div class="panel__avatar">
+                <img
+                    :src="props.data.user.profile_image.medium"
+                    alt=""
+                    class="panel__img"
+                />
+            </div>
+            <div class="panel__text">
+                <div class="panel__name">
+                    {{ props.data.user.name }}
+                </div>
+                <div
+                    class="panel__tag"
+                    v-if="props.data.user.instagram_username !== null"
+                >
+                    @{{ props.data.user.instagram_username }}
+                </div>
+            </div>
+        </div>
+        <div class="panel__buttons">
+            <button
+                class="button button_favourite text"
+                @click="store.toggleFavourite(props.data.id)"
+            >
+                <div class="button__inner">
+                    <img
+                        src="@/assets/img/icons/heart-yellow.svg"
+                        alt=""
+                        srcset=""
+                        v-if="store.checkFavourite(props.data.id)"
+                    />
+                    <img
+                        src="@/assets/img/icons/heart-black.svg"
+                        alt=""
+                        srcset=""
+                        v-else
+                    />
+                </div>
+            </button>
+            <button class="button button_download text" @click="openInNewTab()">
+                <div class="button__inner">
+                    <img
+                        src="@/assets/img/icons/download.svg"
+                        alt=""
+                        srcset=""
+                        class="button__img"
+                    />
+                    <span class="button__text">Download</span>
+                </div>
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.panel {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: max(2.5vw, 40px) 0 max(2vw, 32px) 0;
+}
+
+.panel__author {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.panel__avatar {
+    border-radius: 8px;
+    overflow: hidden;
+    width: 55px;
+    height: 55px;
+}
+
+.panel__name {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: clamp(18px, 2vw, 30px);
+    line-height: 35px;
+}
+
+.panel__text {
+    color: white;
+    padding-left: 10px;
+}
+
+.panel__tag {
+    font-size: clamp(14px, 1.5vw, 18px);
+}
+
+.panel__img {
+    width: 100%;
+    height: auto;
+}
+
+.panel__buttons {
+    height: min-content;
+}
+
+.button {
+    display: inline-block;
+    background: #ffffff;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+    border: 0;
+    cursor: pointer;
+}
+
+.button__inner {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+
+.button__img {
+    margin-right: 16px;
+}
+
+.button_favourite {
+    width: 49.07px;
+    height: 49.07px;
+}
+
+.button_favourite:hover {
+    background: #ccc;
+}
+
+.button_download {
+    width: 206.25px;
+    height: 49.62px;
+    background-color: #fff200;
+
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+
+    margin-left: 20px;
+    color: #000000;
+}
+
+.button_download:hover {
+    background-color: #c5bb00;
+}
+
+@media (max-width: 992px) {
+    .panel__text {
+        color: #000000;
+    }
+    .panel__tag {
+        color: #bdbdbd;
+    }
+}
+
+@media (max-width: 767px) {
+    .button_download,
+    .button_favourite {
+        width: 41px;
+        height: 41px;
+    }
+
+    .button__text {
+        display: none;
+    }
+
+    .button__img {
+        margin: 0;
+    }
+}
+</style>
